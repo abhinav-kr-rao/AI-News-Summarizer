824843c4c722baecbe02e5b7cf4a86c4a5006e29
Abhinav Kumar Rao <shashankr212@gmail.com>
2026-01-29 15:40:37 +0530

    db changed to neon

--- app/database/database.py
+++ app/database/database.py
@@ -14,16 +14,16 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-
+NEON_DATABASE_URL = os.getenv("NEON_DATABASE_URL")
 print("env variables loaded successfully")
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 # print("username is: ", POSTGRES_USER)
 # print("password is: ",POSTGRES_PASSWORD)
 # print("host is: ", POSTGRES_HOST)
 # print("port is: ", POSTGRES_PORT)
 # print("db is: ", POSTGRES_DB)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(str(NEON_DATABASE_URL))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
